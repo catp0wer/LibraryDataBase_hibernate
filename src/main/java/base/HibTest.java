296191e910837aa73441d4ee0f2047341828163d
laila <laila@fake.com>
2018-09-09 21:19:20 +0300

    made refactoring, separated sessioFactory in another class

--- src/main/java/base/HibTest.java
+++ src/main/java/base/HibTest.java
@@ -1,45 +1,18 @@
 package base;
 import db.Author;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-
 
 
 public class HibTest {
     public static void main(String[] args) {
-        Author author1 = new Author();
-        Author author2 = new Author();
-        Author author3 = new Author();
+        Author author1 = new Author(1,"Scott","Kelly","1970");
+        Author author2 = new Author(2,"Stephen","Hawking","1942");
+        Author author3 = new Author(3,"Daniel","Kahneman","1934");
+        Author author4 = new Author(4,"gg","Kahneman","1934");
 
-        author1.setId(1);
-        author1.setFirst_Name("Scott");
-        author1.setLast_Name("Kelly");
-        author1.setYear_of_Birth("1970");
-
-        author2.setId(2);
-        author2.setFirst_Name("Stephen");
-        author2.setLast_Name("Hawking");
-        author2.setYear_of_Birth("1942");
-
-        author3.setId(3);
-        author3.setFirst_Name("Daniel");
-        author3.setLast_Name("Kahneman");
-        author3.setYear_of_Birth("1934");
-
-        Configuration configuration = new Configuration().configure();
-        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
-                .applySettings(configuration.getProperties());
-        SessionFactory sessionFactory = configuration.buildSessionFactory(builder.build());
-
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.save(author1);
-        session.save(author2);
-        session.save(author3);
-        session.getTransaction().commit();
-        session.close();
-
+        DataManipulation dm = new DataManipulation();
+        dm.Insert(author1);
+        dm.Insert(author2);
+        dm.Insert(author3);
+        dm.Insert(author4);
     }
 }
